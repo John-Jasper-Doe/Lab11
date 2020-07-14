579f4f39ad1aec8272eada5e3d82663ae8a7663a
John Doe <john.jasper.doe@gmail.com>
2020-07-14 12:38:38 +0300

    Updated conect function

--- lib/libasync/src/async/async.cpp
+++ lib/libasync/src/async/async.cpp
@@ -5,16 +5,38 @@
  * @date 2020
  */
 
+#include <memory>
+#include <map>
+#include <mutex>
+
 #include <libasync/async/async.hpp>
+#include <libasync/core/context.hpp>
 
 /** @brief The namespace of the Async Library project */
 namespace libasync {
 /** @brief The namespace of the Async */
 namespace async {
 
+using raw_context_ptr_t = core::context*;
+using context_ptr_t = std::unique_ptr<core::context>;
+
+namespace {
+
+/* Context */
+static std::map<raw_context_ptr_t, context_ptr_t> contexts;
+static std::mutex mtx;
+
+} /* :: */
+
 handle_t connect(std::size_t bulk) {
-  (void)bulk;
-  return nullptr;
+  context_ptr_t ptr = std::make_unique<core::context>(bulk);
+  raw_context_ptr_t raw_ptr = ptr.get();
+
+  std::unique_lock<std::mutex> lock(mtx);
+  contexts.emplace(std::make_pair(raw_ptr, std::move(ptr)));
+  lock.unlock();
+
+  return reinterpret_cast<handle_t>(raw_ptr);
 }
 
 void receive(handle_t handle, const char* data, std::size_t size) {
