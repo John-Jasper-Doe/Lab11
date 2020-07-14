b6d3dfe37851a88f2df923a76df8deb955da7c48
John Doe <john.jasper.doe@gmail.com>
2020-07-14 20:10:16 +0000

    Update version.cmake

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,6 +2,6 @@
 # This makefile will generate a new version.hpp, *AMEND THE MOST RECENT
 # COMMIT*, and git-tag the commit.
 
-set(BULKMT_MAJOR 0)
-set(BULKMT_MINOR 0)
-set(BULKMT_PATCH 1)
+set(ASYNC_MAJOR 0)
+set(ASYNC_MINOR 0)
+set(ASYNC_PATCH 1)
