5a77eb9e547a67a7e0acd625d4d174035c13d2f1
John Doe <john.jasper.doe@gmail.com>
2020-07-14 12:07:56 +0300

    Added context.cpp

---
+++ lib/libasync/src/core/context.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file context.cpp
+ * @brief Implimentation of the class "Context".
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+#include <libasync/core/context.hpp>
+
+/** @brief The namespace of the Async Library project */
+namespace libasync {
+/** @brief The namespace of the Core */
+namespace core {
+
+} /* core:: */
+} /* libasync:: */
